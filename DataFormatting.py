b5dbf37eb71d988627d436ec0ea9b4ceb4c6d65f
Rakshaanth <Rakshaanth007@gmail.com>
2025-12-27 15:29:29 -0800

    DataFormatting.py added changes in formatting and spam column removal.

--- DataFormatting.py
+++ DataFormatting.py
@@ -1,17 +1,44 @@
-# import pandas as pd
+import pandas as pd
 
-# class DataFormatter:
-#     def cDuplicateDetector(self, df):  
-#         count = 0  
-#         print([df.columns])
+class DataFormatter:
+    def showColumns(self, df):  
+        count = 0  
+        for col in range(len(df.columns)):  
+            print(f"{col}: {df.columns[col]}")
+    
+    def numericToFloat(self, df):  
+        ''' Convert numeric columns to float type for consistency '''
+        numeric_cols = df.select_dtypes(include=['number']).columns  
+        df[numeric_cols] = df[numeric_cols].astype(float)  
+        print(f"Converted {len(numeric_cols)} numeric columns to float type.\n out of {len(df.columns)} ")
+        nonNumeric_cols = []
+        for col in df.columns:
+            if df[col].dtype != 'float64':
+                nonNumeric_cols.append(col)
+        return nonNumeric_cols
+    
+    def dropSpams(self, df):
+        ''' Drop non-quantitative columns, that were not converted to float type '''
+        nonNumeric_cols = self.numericToFloat(df)
+        for col in nonNumeric_cols:
+            if col == "Unnamed: 0":
+                continue
+            df = df.drop(columns=[col])
+            print(f"Dropped column: {col}")
 
 
 
 
-# if __name__ == "__main__":
-#     Format = DataFormatter()
-#     Format.cDuplicateDetector(pd.read_csv(r"C:\Users\Dell\Desktop\Python baby!\Stage 3\data\processed\ORCL_merged.csv"))
 
+
+
+if __name__ == "__main__":
+    Format = DataFormatter()
+    # Format.showColumns(pd.read_csv(r"C:\Users\Dell\Desktop\Python baby!\Stage 3\data\processed\ORCL_merged.csv"))
+    # print("\n")
+    # Format.dropCurrencyColumn(pd.read_csv(r"C:\Users\Dell\Desktop\Python baby!\Stage 3\data\processed\ORCL_merged.csv"))
+    # print("\n")
+    Format.dropSpams(pd.read_csv(r"C:\Users\Dell\Desktop\Python baby!\Stage 3\data\processed\ORCL_merged.csv"))
 
 
 
